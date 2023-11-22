d0b11d0b1b439e2ff9cd12b095bb2946d38edf00
ChanMor <morelos.franzchristian@gmail.com>
2023-11-22 21:26:39 +0800

    created function for createImageView and createHBox and createVBox

--- JavaGame/src/game/GameStage.java
+++ JavaGame/src/game/GameStage.java
@@ -1,15 +1,15 @@
 package game;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
+import javafx.scene.effect.DropShadow;
+
 
 public class GameStage 
 {
@@ -60,7 +60,7 @@ public class GameStage
 	
 	private void initMain() 
 	{
-		Image bg = new Image("file:src/images/bg_main.jpg");
+	   Image bg = new Image("file:src/images/bg_main.jpg");
 
 	    StackPane root = new StackPane();
 
@@ -70,33 +70,59 @@ public class GameStage
 	    imageView.setFitHeight(GAME_HEIGHT);
 
 	    // Create buttons
-	    ImageView playImageView = createImageView("file:src/images/play_button.png");
+	    ImageView playImageView = createImageView("file:src/images/play_button.png", 280, 80);
 	    playImageView.setOnMouseClicked(event -> sendToPlay());
 
-	    ImageView aboutImageView = createImageView("file:src/images/about_button.png");
+	    ImageView aboutImageView = createImageView("file:src/images/about_button.png", 130, 60);
 	    aboutImageView.setOnMouseClicked(event -> sendToAbout());
 
-	    ImageView tutorialImageView = createImageView("file:src/images/tutorial_button.png");
+	    ImageView tutorialImageView = createImageView("file:src/images/btn_tutorial.png", 130, 60);
 	    tutorialImageView.setOnMouseClicked(event -> sendToTutorial());
+	    tutorialImageView.setFitWidth(130); // Set the tutorial button width
+	    tutorialImageView.setFitHeight(60); // Set the tutorial button height
 
-	    // Create a VBox to center the buttons vertically
-	    VBox buttonVBox = new VBox();
-	    buttonVBox.setAlignment(Pos.CENTER);
-	    buttonVBox.getChildren().addAll(playImageView, aboutImageView, tutorialImageView);
+	    // Create an HBox for "About" and "Tutorial" buttons
+	    HBox aboutTutorialHBox = createHBox(-10, aboutImageView, tutorialImageView, Pos.CENTER);
 
+	    // Create a VBox to organize buttons
+	    VBox buttonVBox = createVBox(-5, playImageView, aboutTutorialHBox, Pos.CENTER); // Set vertical spacing between buttons
+	    buttonVBox.setTranslateY(140);
+	    
 	    // Add background image and buttons to StackPane
 	    root.getChildren().addAll(imageView, buttonVBox);
+
 	    
 	    this.mainScene = new Scene(root);
     }
 
-    private ImageView createImageView(String imageUrl) {
+    private ImageView createImageView(String imageUrl, int a, int b) {
         Image image = new Image(imageUrl);
         ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(100); // Adjust the size as needed
-        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(a); // Adjust the size as needed
+        imageView.setFitHeight(b); // Adjust the size as needed
+        imageView.setPreserveRatio(false);
+        
+        // Add drop shadow effect on hover
+        DropShadow dropShadow = new DropShadow();
+        imageView.setOnMouseEntered(event -> imageView.setEffect(dropShadow));
+        imageView.setOnMouseExited(event -> imageView.setEffect(null));
+        
         return imageView;
     }
+	
+	private HBox createHBox(int spacing, ImageView btn1, ImageView btn2, Pos alignment) {
+		HBox newHBox = new HBox(spacing);
+		newHBox.getChildren().addAll(btn1, btn2);
+		newHBox.setAlignment(alignment);
+		return newHBox;
+	}
+	
+	private VBox createVBox(int spacing, ImageView btn1, HBox btn2, Pos alignment) {
+		VBox newHBox = new VBox(spacing);
+		newHBox.getChildren().addAll(btn1, btn2);
+		newHBox.setAlignment(alignment);
+		return newHBox;
+	}
 
 	private void initPlay()
 	{
@@ -109,7 +135,7 @@ public class GameStage
 
 	    this.playScene = new Scene(root);
 	    
-	    ImageView backImageView = createImageView("file:src/images/tutorial_button.png");
+	    ImageView backImageView = createImageView("file:src/images/btn_back.png", 130, 60);
 	    backImageView.setOnMouseClicked(event -> sendToMain());
 
 	    // Create a VBox to center the buttons vertically
@@ -132,7 +158,7 @@ public class GameStage
 
 	    this.aboutScene = new Scene(root);
 
-	    ImageView backImageView = createImageView("file:src/images/tutorial_button.png");
+	    ImageView backImageView = createImageView("file:src/images/btn_back.png", 130, 60);
 	    backImageView.setOnMouseClicked(event -> sendToMain());
 
 	    // Create a VBox to center the buttons vertically
@@ -156,7 +182,7 @@ public class GameStage
 	    this.tutorialScene = new Scene(root);
 
 	    
-	    ImageView backImageView = createImageView("file:src/images/tutorial_button.png");
+	    ImageView backImageView = createImageView("file:src/images/btn_back.png", 130, 60);
 	    backImageView.setOnMouseClicked(event -> sendToMain());
 
 	    // Create a VBox to center the buttons vertically
@@ -168,19 +194,4 @@ public class GameStage
 	    root.getChildren().addAll(imageView, buttonVBox);
 	}
 	
-	private Canvas createCanvas(Image bg) {
-    	Canvas canvas = new Canvas(GameStage.GAME_WIDTH,GameStage.GAME_HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        
-        gc.drawImage(bg, 0, 0);
-        return canvas;
-    }
-	
-	private VBox createVBox() {
-    	VBox vbox = new VBox();
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setPadding(new Insets(10));
-        vbox.setSpacing(8);        
-        return vbox;
-    }
 }
